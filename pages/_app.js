f3548886ab1cd545425157b8101b7b08c6099015
Glevvv <146816404+Glevvv@users.noreply.github.com>
2025-08-15 15:48:47 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import Script from 'next/script';
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script id="adsbygoogle-init" strategy="afterInteractive" async src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-YOUR-ADSENSE-PUB-ID`} crossOrigin="anonymous" />
+      <Script id="adsbygoogle-init" strategy="afterInteractive" async src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5735021755991385`} crossOrigin="anonymous" />
       <Component {...pageProps} />
     </>
   );
